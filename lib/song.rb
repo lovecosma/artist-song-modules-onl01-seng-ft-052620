96da06e6ef0bb95f77d6e5296766c679ca70c107
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-15 04:08:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Song
   extend Memorable::ClassMethods
-    extend Findable::ClassMethods
+  extend Findable::ClassMethods
   include Memorable::InstanceMethods
   include Paramable::InstanceMethods
   @@songs = []
