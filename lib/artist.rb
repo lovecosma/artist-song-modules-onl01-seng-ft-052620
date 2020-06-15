8b23facb264e40133ff695fb79664dc25cd3593b
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-15 03:30:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,11 +3,11 @@ require 'pry'
 class Artist
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
- 
+
   attr_accessor :name
   attr_reader :songs
 
-  @@artists = []
+  self.class.artists = []
 
   def initialize
     super
