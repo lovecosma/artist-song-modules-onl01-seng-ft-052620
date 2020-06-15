23d9a65eedac7a4ba47353a3192a53b2b0c65f17
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-15 04:15:58 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,8 +1,8 @@
 require 'pry'
 module Findable
   module ClassMethods
-    def find_by_name(name)
-       self.class.all.detect{|a| a.name == name}
-    end
+    def self.find_by_name(name)
+    @@artists.detect {|a| a.name == name}
+  end
   end
 end
